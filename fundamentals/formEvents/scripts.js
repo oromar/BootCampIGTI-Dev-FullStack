92d81e0273d7774c95128368cd1dc0c973042036
DESKTOP-INVG9UQ\oroma <oromar.melo@gmail.com>
2020-05-16 10:33:25 -0300

    Changes

--- fundamentals/formEvents/scripts.js
+++ fundamentals/formEvents/scripts.js
@@ -1,9 +1,9 @@
 // console.log('Hi')
 // window.addEventListener('load', start)
 const charCounter = document.querySelector('#charCount')
-const input  = document.querySelector('input[type=text]')
+const input = document.querySelector('input[type=text]')
 input.addEventListener('keyup', (evt) => {
-    charCounter.textContent = evt?.target?.value?.length;
+  charCounter.textContent = evt?.target?.value?.length
 })
 const form = document.querySelector('form')
 form.addEventListener('submit', (evt) => evt.preventDefault())
