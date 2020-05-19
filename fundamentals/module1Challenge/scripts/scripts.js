1141e909825df0f920258ea140850814462cb22f
DESKTOP-INVG9UQ\oroma <oromar.melo@gmail.com>
2020-05-19 11:19:36 -0300

    Using const instead let to filtered list

--- fundamentals/module1Challenge/scripts/scripts.js
+++ fundamentals/module1Challenge/scripts/scripts.js
@@ -1,29 +1,27 @@
 window.addEventListener('load', async () => {
+  const ENTER_KEY = 13
   const response = await fetch(
     'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo'
   )
   const json = await response.json()
   const data = json.results
-  const people = data.map((e) => ({
-    name: `${e.name.first} ${e.name.last}`,
-    picture: e.picture.thumbnail,
-    age: e.dob.age,
-    gender: e.gender,
+  const people = data.map((item) => ({
+    name: `${item.name.first} ${item.name.last}`,
+    picture: item.picture.thumbnail,
+    age: item.dob.age,
+    gender: item.gender,
   }))
-
-  const defaultMessageUserList =
-    '<div> <h2 class="info-title">Nenhum usuário filtrado</h2></div>'
-  const defaultMessageStats =
-    '<div> <h2 class="info-title">Nada a ser exibido</h2></div>'
 
   const input = document.querySelector('#q')
   const searchButton = document.querySelector('#searchButton')
   const list = document.querySelector('#list')
   const stats = document.querySelector('#stats')
 
-  const defaultData = () => {
-    list.innerHTML = defaultMessageUserList
-    stats.innerHTML = defaultMessageStats
+  const renderDefaultData = () => {
+    list.innerHTML =
+      '<div> <h2 class="info-title">Nenhum usuário filtrado</h2></div>'
+    stats.innerHTML =
+      '<div> <h2 class="info-title">Nada a ser exibido</h2></div>'
   }
 
   const renderUsers = (filtered) => {
@@ -38,15 +36,13 @@ window.addEventListener('load', async () => {
             <p>${item.name}, ${item.age} anos</p>
         </div>`
       )
-    let data = `<div> <h2 class="info-title">${filtered.length} usuário(s) encontrado(s)</h2>`
-    data += items.join(' ')
-    data += '</div>'
-    list.innerHTML = data
+    const data = `<div> <h2 class="info-title">${filtered.length} usuário(s) encontrado(s)</h2>`
+    list.innerHTML = data + items.join('') + '</div>'
   }
 
   const renderStats = (filtered) => {
     const ages = filtered.reduce((total, current) => total + current.age, 0)
-    const agesMedia = ages / filtered.length
+    const agesMedia = (ages / filtered.length).toFixed(2)
     const males = filtered.filter((person) => person.gender === 'male').length
     const females = filtered.length - males
     stats.innerHTML = `
@@ -63,25 +59,24 @@ window.addEventListener('load', async () => {
 
   const performSearch = (value) => {
     list.innerHTML = ''
+    stats.innerHTML = ''
     if (value === '') {
-      defaultData()
+      renderDefaultData()
       return
     }
-    let filtered = people.filter(
+    const filtered = people.filter(
       (person) => person.name.toLowerCase().indexOf(value.toLowerCase()) !== -1
     )
     if (filtered.length > 0) {
       renderUsers(filtered)
       renderStats(filtered)
     } else {
-      defaultData()
+      renderDefaultData()
     }
   }
+  input.addEventListener('keyup', (evt) =>
+    evt.keyCode == ENTER_KEY ? performSearch(evt.target.value) : null
+  )
   searchButton.addEventListener('click', () => performSearch(input.value))
-  input.addEventListener('keyup', (evt) => {
-    if (evt.keyCode == 13) {
-      performSearch(evt.target.value)
-    }
-  })
-  defaultData()
+  renderDefaultData()
 })
