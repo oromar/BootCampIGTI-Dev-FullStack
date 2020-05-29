02c2035262af4036c9a0171f5a3be6c51e39d771
DESKTOP-INVG9UQ\oroma <oromar.melo@gmail.com>
2020-05-29 09:37:26 -0300

    adjusts

--- module02 - node/basics/helloexpress.js
+++ module02 - node/basics/helloexpress.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const PORT = 8080
 const app = express()
-const handler = (req, res) => res.send('Hello Express!!!')
+const handler = (req, res) => res.send('Hello Express!!')
 app.get('/', handler)
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}...`))
