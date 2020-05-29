ea553b737f9b9dfc42e26c0f9f8d35b4d3274030
DESKTOP-INVG9UQ\oroma <oromar.melo@gmail.com>
2020-05-29 18:38:36 -0300

    refatoring to promises

--- module02 - node/my-bank-api/routes/accounts.js
+++ module02 - node/my-bank-api/routes/accounts.js
@@ -1,122 +1,97 @@
 import express from 'express'
-import fs from 'fs'
+import { readFile, writeFile } from 'fs/promises'
 import { FILE_NAME, statusCode } from '../constants.js'
 const accountsRouter = express.Router()
 
-accountsRouter.get('/', (req, res) => {
-  const handler = (err, fileData) => {
-    if (err) res.status(500).send(err.message)
-    const json = JSON.parse(fileData)
-    res.send(json.accounts)
-  }
-  fs.readFile(FILE_NAME, { encoding: 'UTF-8' }, handler)
+const writeData = async (json) =>
+  await writeFile(FILE_NAME, JSON.stringify(json))
+
+const openFile = async () => {
+  const fileData = await readFile(FILE_NAME, { encoding: 'UTF-8' })
+  return JSON.parse(fileData)
+}
+
+accountsRouter.get('/', async (req, res) => {
+  const json = await openFile()
+  res.send(json.accounts)
 })
 
-accountsRouter.get('/:id', (req, res) => {
-  const handler = (err, fileData) => {
-    if (err) res.status(500).send(err.message)
-    const json = JSON.parse(fileData)
-    const result = json.accounts.find((item) => item.id === +req.params.id)
-    if (result) {
-      res.send(result)
+accountsRouter.get('/:id', async (req, res) => {
+  const json = await openFile()
+  const result = json.accounts.find((item) => item.id === +req.params.id)
+  if (result) {
+    res.send(result)
+  } else {
+    res.status(statusCode.NOT_FOUND).end()
+  }
+})
+
+accountsRouter.post('/', async (req, res) => {
+  const json = await openFile()
+  const id = Math.max(...json.accounts.map((i) => i.id)) + 1
+  json.accounts.push({ ...req.body, id })
+  await writeData(json)
+  res.status(statusCode.OK).end()
+})
+
+accountsRouter.put('/:id', async (req, res) => {
+  const json = await openFile()
+  const index = json.accounts.findIndex((item) => item.id === +req.params.id)
+  if (index > -1) {
+    json.accounts[index] = { ...req.body, id: +req.params.id }
+    await writeData(json)
+    res.statusCode = statusCode.OK
+  } else {
+    res.statusCode = statusCode.NOT_FOUND
+  }
+  res.end()
+})
+
+accountsRouter.delete('/:id', async (req, res) => {
+  const json = await openFile()
+  const account = json.accounts.find((item) => item.id === +req.params.id)
+  if (account) {
+    json.accounts = json.accounts.filter((item) => item.id !== +req.params.id)
+    await writeData(json)
+    res.statusCode = statusCode.OK
+  } else {
+    res.statusCode = statusCode.NOT_FOUND
+  }
+  res.end()
+})
+
+accountsRouter.post('/withdraw/:id/:amount', async (req, res) => {
+  const json = await openFile()
+  const index = json.accounts.findIndex((item) => item.id === +req.params.id)
+  if (index > -1) {
+    if (json.accounts[index].balance >= req.params.amount) {
+      json.accounts[index].balance -= +req.params.amount
+      await writeData(json)
+      res.statusCode = statusCode.OK
     } else {
-      res.status(statusCode.NOT_FOUND).end()
+      res.statusCode = statusCode.BAD_REQUEST
     }
+  } else {
+    res.statusCode = statusCode.NOT_FOUND
   }
-  fs.readFile(FILE_NAME, { encoding: 'UTF-8' }, handler)
+  res.end()
 })
 
-accountsRouter.post('/', (req, res) => {
-  const handler = (err, fileData) => {
-    if (err) res.status(500).send(err.message)
-    const json = JSON.parse(fileData)
-    const id = Math.max(...json.accounts.map((i) => i.id)) + 1
-    json.accounts.push({ ...req.body, id })
-    fs.writeFile(FILE_NAME, JSON.stringify(json), () => {
-      res.status(statusCode.OK).end()
-    })
-  }
-  fs.readFile(FILE_NAME, { encoding: 'UTF-8' }, handler)
-})
-
-accountsRouter.put('/:id', (req, res) => {
-  const handler = (err, fileData) => {
-    if (err) res.status(500).send(err.message)
-    const json = JSON.parse(fileData)
-    const index = json.accounts.findIndex((item) => item.id === +req.params.id)
-    if (index > -1) {
-      json.accounts[index] = { ...req.body, id: +req.params.id }
-      fs.writeFile(FILE_NAME, JSON.stringify(json), () => {
-        res.statusCode = statusCode.OK
-      })
-    } else {
-      res.statusCode = statusCode.NOT_FOUND
-    }
-    res.end()
-  }
-  fs.readFile(FILE_NAME, { encoding: 'UTF-8' }, handler)
-})
-
-accountsRouter.delete('/:id', (req, res) => {
-  const handler = (err, fileData) => {
-    if (err) res.status(500).send(err.message)
-    const json = JSON.parse(fileData)
-    const account = json.accounts.find((item) => item.id === +req.params.id)
-    if (account) {
-      json.accounts = json.accounts.filter((item) => item.id !== +req.params.id)
-      fs.writeFile(FILE_NAME, JSON.stringify(json), () => {
-        res.statusCode = statusCode.OK
-      })
-    } else {
-      res.statusCode = statusCode.NOT_FOUND
-    }
-    res.end()
-  }
-  fs.readFile(FILE_NAME, { encoding: 'UTF-8' }, handler)
-})
-
-accountsRouter.post('/withdraw/:id/:amount', (req, res) => {
-  const handler = (err, fileData) => {
-    if (err) res.status(500).send(err.message)
-    const json = JSON.parse(fileData)
-    const index = json.accounts.findIndex((item) => item.id === +req.params.id)
-    if (index > -1) {
-      if (json.accounts[index].balance >= req.params.amount) {
-        json.accounts[index].balance -= +req.params.amount
-        fs.writeFile(FILE_NAME, JSON.stringify(json), () => {
-          res.statusCode = statusCode.OK
-        })
-      } else {
-        res.statusCode = statusCode.BAD_REQUEST
-      }
-    } else {
-      res.statusCode = statusCode.NOT_FOUND
-    }
-    res.end()
-  }
-  fs.readFile(FILE_NAME, { encoding: 'UTF-8' }, handler)
-})
-
-accountsRouter.post('/deposit/:id/:amount', (req, res) => {
+accountsRouter.post('/deposit/:id/:amount', async (req, res) => {
   if (req.params.amount < 0) {
     res.statusCode = statusCode.BAD_REQUEST
     res.end()
   }
-  const handler = (err, fileData) => {
-    if (err) res.status(500).send(err.message)
-    const json = JSON.parse(fileData)
-    const index = json.accounts.findIndex((item) => item.id === +req.params.id)
-    if (index > -1) {
-      json.accounts[index].balance += +req.params.amount
-      fs.writeFile(FILE_NAME, JSON.stringify(json), () => {
-        res.statusCode = statusCode.OK
-      })
-    } else {
-      res.statusCode = statusCode.NOT_FOUND
-    }
-    res.end()
+  const json = await openFile()
+  const index = json.accounts.findIndex((item) => item.id === +req.params.id)
+  if (index > -1) {
+    json.accounts[index].balance += +req.params.amount
+    await writeData(json)
+    res.statusCode = statusCode.OK
+  } else {
+    res.statusCode = statusCode.NOT_FOUND
   }
-  fs.readFile(FILE_NAME, { encoding: 'UTF-8' }, handler)
+  res.end()
 })
 
 export default accountsRouter
