9e164f3385cbb1502a892f12b7e0e4427a760a96
DESKTOP-INVG9UQ\oroma <oromar.melo@gmail.com>
2020-05-29 08:38:52 -0300

    refactoring

--- module02 - node/basics/hellonode.js
+++ module02 - node/basics/hellonode.js
@@ -1,6 +1,6 @@
-import { createServer } from 'http'
+const http = require('http')
 const PORT = 8080
-const server = createServer((req, res) => {
+const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.write('Hello Node!!')
   res.end()
